de4e5b769c62a907845b7c1e02f51d5265dcb553
Kassy729 <hamtol98@gmail.com>
2021-09-02 19:10:38 +0900

    vue-posts

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Post;
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,21 +10,26 @@ class PostController extends Controller
 {
     public function store(Request $request)
     {
+
+        // dd($request);
+
         $title = $request->title;
         $content = $request->content;
-
-        $request->validate([  //빈칸일 경우 제출되지 않도록 막아줌
-            'title' => 'required|min:3',  //최소 3글자 이상
-            'content' => 'required',
-            'imageFile' => 'image|max:2000'
-        ]);
+        return $title;
+        // $request->validate([  //빈칸일 경우 제출되지 않도록 막아줌
+        //     'title' => 'required|min:3',  //최소 3글자 이상
+        //     'content' => 'required',
+        //     'imageFile' => 'image|max:2000'
+        // ]);
 
         //DB에 저장
         $post = new Post();
+        return $post;
         $post->title = $title;
         $post->content = $content;
-        $post->user_id = Auth::user()->id;
+        $post->user_id = 1;
 
         $post->save();
+        return $post;
     }
 }
